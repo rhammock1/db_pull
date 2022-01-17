641dfd164bcd32f10b193d05a0025936edf2ed2d
Robert Hammock <rhamm1027@gmail.com>
2022-01-17 12:41:00 -0500

    tabs to spaces

--- db_pull.sh
+++ db_pull.sh
@@ -10,8 +10,8 @@ DB_NAME=""
 FORCE=""
 
 if [[ $2 == --force ]]; then
-	echo "Forcing script execution"
-	FORCE="y"
+  echo "Forcing script execution"
+  FORCE="y"
 fi
 
 getAllNames() {
@@ -19,60 +19,60 @@ getAllNames() {
 }
 
 dropIfDBExists() {
-	# check if db already exists
-	if [[ `getAllNames | grep -w $1` ]]; then
-		if [[ -z $FORCE ]]; then
-			read -p "Database already exists. Do you want to drop the old database? ('y' to continue) " PROMPT
-			if [ ! "$PROMPT" = "y" ] ; then
-				echo "Exiting... "
-				exit
-			fi
-		fi
-		echo "Dropping old database... "
-		dropdb $1
-	fi
+  # check if db already exists
+  if [[ `getAllNames | grep -w $1` ]]; then
+    if [[ -z $FORCE ]]; then
+      read -p "Database already exists. Do you want to drop the old database? ('y' to continue) " PROMPT
+      if [ ! "$PROMPT" = "y" ] ; then
+        echo "Exiting... "
+        exit
+      fi
+    fi
+    echo "Dropping old database... "
+    dropdb $1
+  fi
 }
 
 dropIfTooMany() {
-	# check if any db need to be dropped to make room in memory
-	COUNT_DB=`getAllNames | grep -c -e '[A-Za-z0-9]'`
-	if [[ $COUNT_DB -ge 9 ]]; then # counts the number of non empty lines and checks if >= 9
-		# can prolly have more than 9, but it seems to be a good limit
-		echo "There are $COUNT_DB databases already created. Getting ready to drop the oldest..."
-		DB_DROP=`getAllNames | awk 'FNR <= 1'` # gets the first db in list
-		if [[ -z $FORCE ]]; then
-			read -p "About to drop $DB_DROP database. Do you want to drop this database? ('y' to continue) " PROMPT
-			if [ ! "$PROMPT" = "y" ] ; then
-				echo "Exiting... "
-				exit
-			fi
-		fi
-		echo "Dropping $DB_DROP database... "
-		dropdb $DB_DROP 
-	fi
+  # check if any db need to be dropped to make room in memory
+  COUNT_DB=`getAllNames | grep -c -e '[A-Za-z0-9]'`
+  if [[ $COUNT_DB -ge 9 ]]; then # counts the number of non empty lines and checks if >= 9
+    # can prolly have more than 9, but it seems to be a good limit
+    echo "There are $COUNT_DB databases already created. Getting ready to drop the oldest..."
+    DB_DROP=`getAllNames | awk 'FNR <= 1'` # gets the first db in list
+    if [[ -z $FORCE ]]; then
+      read -p "About to drop $DB_DROP database. Do you want to drop this database? ('y' to continue) " PROMPT
+      if [ ! "$PROMPT" = "y" ] ; then
+        echo "Exiting... "
+        exit
+      fi
+    fi
+    echo "Dropping $DB_DROP database... "
+    dropdb $DB_DROP 
+  fi
 }
 
 createNew() {
-	dropIfTooMany
+  dropIfTooMany
 
-	if [[ $2 ]]; then # if 2nd argument, then use 1st as template
-		echo "Creating new database named $2 with template $1"
-		createdb -T $1 $2
-	else
-		dropIfDBExists $1 # database will already exist if we are trying to use it as a template
-		echo "Creating new database with name: $1"
-		createdb $1
-	fi
+  if [[ $2 ]]; then # if 2nd argument, then use 1st as template
+    echo "Creating new database named $2 with template $1"
+    createdb -T $1 $2
+  else
+    dropIfDBExists $1 # database will already exist if we are trying to use it as a template
+    echo "Creating new database with name: $1"
+    createdb $1
+  fi
 }
 
 echo "Getting ready... "
 if [ -z $FORCE ]; then
-	read -p "New database name? " DB_NAME
+  read -p "New database name? " DB_NAME
 fi
 
 if [ -z "$DB_NAME" ] ; then
-	echo "No database name entered. Creating based on date"
-	DB_NAME="node_$(date +%m_%d_%y)" # formats new name as node_MM_DD_YY
+  echo "No database name entered. Creating based on date"
+  DB_NAME="node_$(date +%m_%d_%y)" # formats new name as node_MM_DD_YY
 fi
 
 createNew $DB_NAME
