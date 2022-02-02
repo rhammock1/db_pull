92ca4f3b6e69d36d9edb5b7fe18e3dc42d0fedb3
Robert Hammock <rhamm1027@gmail.com>
2022-02-02 14:46:31 -0500

    directory

--- test_pull.sh
+++ test_pull.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
-
+__dir="$(cd "$(dirname "$0")" && pwd)"
+echo $__dir
 # clear previous log contents
-> pull.log
+> $__dir/pull.log
 
 # Find all lines matching '# FINDME' and prepend a # to the line
-sed -e '/# FINDME/s/^/#/' db_pull.sh >> temp.sh
+sed -e '/# FINDME/s/^/#/' $__dir/db_pull.sh >> temp.sh
 
 # test the script w/o any big commands running
-bash temp.sh ./ --force >> pull.log
+bash temp.sh ./ --force >> $__dir/pull.log
 
 # cleanup
 rm temp.sh
